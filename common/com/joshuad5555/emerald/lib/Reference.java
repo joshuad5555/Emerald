89478d1186e2b96800bf7f16e12f9915eda0fddb
joshuad5555 <joshuad5555@gmail.com>
2013-10-22 20:19:07 -0700

    Create Reference.java

---
+++ common/com/joshuad5555/emerald/lib/Reference.java
@@ -0,0 +1,13 @@
+package com.joshuad5555.emerald.lib;
+
+public class Reference {
+
+    public static final String COMMON_PROXY_PATH = "com.joshuad5555.emerald.proxy.CommonProxy";
+    public static final String CLIENT_PROXY_PATH = "com.joshuad5555.emerald.proxy.ClientProxy";
+    
+    public static final String EMERALD_HELMET_TEXTURE = "joshuad5555_emerald:textures/models/armor/emerald_layer_1.png";
+    public static final String EMERALD_CHESTPLATE_TEXTURE = "joshuad5555_emerald:textures/models/armor/emerald_layer_1.png";
+    public static final String EMERALD_LEGGINGS_TEXTURE = "joshuad5555_emerald:textures/models/armor/emerald_layer_2.png";
+    public static final String EMERALD_BOOTS_TEXTURE = "joshuad5555_emerald:textures/models/armor/emerald_layer_1.png";
+	
+}
