9863229a68dcd01762d49061779929746379653c
joshuad5555 <joshuad5555@gmail.com>
2013-10-23 19:44:18 -0700

    Update ClientProxy.java

--- common/com/joshuad5555/emerald/proxy/ClientProxy.java
+++ common/com/joshuad5555/emerald/proxy/ClientProxy.java
@@ -1,5 +1,12 @@
 package com.joshuad5555.emerald.proxy;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
 public class ClientProxy extends CommonProxy {
 
+	@Override
+	public int addArmor(String armor) {
+	    return RenderingRegistry.addNewArmourRendererPrefix(armor);
+	}
+	
 }
